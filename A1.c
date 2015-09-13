814baaa7e5cb3042c95203181ab55ef893b9af46
Dan Wilder <sentient.hdd@gmail.com>
2015-09-13 15:49:24 -0500

    Add printArray in A1.c

--- A1.c
+++ A1.c
@@ -9,7 +9,22 @@
 #include <stdio.h>
 #include "sort_algorithms.h"
 
+void printArray(int [], int);
+
 int main() {
   return 0;
 }
 
+void printArray(int arr[], int size) {
+/* Print in square table (10x10) with columns aligned
+ */
+  int i;
+
+  for (i=0; i < size; i++) {
+    printf("%5d ", arr[i]);
+    if ((i+1)%10 == 0)
+      printf("\n");
+  }
+
+  printf("\n");
+}
