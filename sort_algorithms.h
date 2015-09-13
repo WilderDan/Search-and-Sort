136ca450b9075735cd4ddca926073c3219901bbc
Dan Wilder <sentient.hdd@gmail.com>
2015-09-13 15:24:45 -0500

    Add comment sort_algorithms

--- sort_algorithms.h
+++ sort_algorithms.h
@@ -1,3 +1,11 @@
+/*  Author: 		Dan Wilder
+ *
+ *  School:		University of Missouri - St. Louis
+ *  Semester:		Fall 2015
+ *  Class: 		CS 3130 - Design and Analysis of Algorithms   
+ *  Instructor:		Galina N. Piatnitskaia
+ */
+
 #ifndef SORT_ALGORITHMS_H
 #define SORT_ALGORITHMS_H
 
