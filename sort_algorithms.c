a919d4dcbd4286c675d4ef6d720dd80b71be3614
Dan Wilder <sentient.hdd@gmail.com>
2015-09-13 15:33:31 -0500

    Change name of parameter in bubbleSort

--- sort_algorithms.c
+++ sort_algorithms.c
@@ -14,12 +14,12 @@ void swap(int *x, int *y) {
   *y = temp;
 }
 
-void bubbleSort(int arr[], int n) {
-/* with swaps counting. n is the size of arr.
+void bubbleSort(int arr[], int size) {
+/* with swaps counting.
  */
   int i, j;
-  for (i = 0; i <= n-1; ++i)      
-    for (j = n; j >= i+1; --j)
+  for (i = 0; i <= size-1; ++i)      
+    for (j = size; j >= i+1; --j)
       if (arr[j] < arr[j-1])
         swap(&arr[j], &arr[j-1]);
 }
