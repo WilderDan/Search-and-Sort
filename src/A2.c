c4442cac9c4d60d4feb2e6df0a768078735ba53c
Dan Wilder <sentient.hdd@gmail.com>
2015-09-17 18:54:22 -0500

    Almost done; bug with free

--- src/A2.c
+++ src/A2.c
@@ -20,23 +20,74 @@
 #include "../include/sort_algorithms.h"
 #include "../include/array_utilities.h"
 
-#define MAX_VAL   9999
-#define NUM_SIZES 4
-const int SIZES[NUM_SIZES] = {500, 1000, 2500, 5000};
+// Parameters:
+#define MAX_VAL  9999
+#define NUM_RUNS 10
+const int SIZES[] = {500, 1000, 2500, 5000};
+
+#define NUM_SORT_FUNCS 4
+
+// Array indices:
+#define BUBBLE         0
+#define COCKTAIL       1
+#define SHELLSORT_1    2
+#define SHELLSORT_2    3
+
+double average_CPU_time(void (*sortFuncPtr)(int [], int), 
+                        int arr[], int size, int numRuns) {
+/* 1) Randomly Initialize 'arr' with 'size' elements.  
+ * 2) Call 'sort_function' with 'arr' and 'size' as arguments
+ * 3) Record cpu time taken
+ * 4) Repeat steps 1-3 'numRuns' times
+ * 5) Return average
+ */
+
+  int i; 
+  double total = 0.0;
+  clock_t start, end;
+
+  for (i = 0; i < numRuns; ++i) {
+    randomInitialize(arr, size, MAX_VAL);
+    start = clock();
+    (*sortFuncPtr)(arr, size);    
+    end = clock();
+    total += ((double) (end - start)) / CLOCKS_PER_SEC;  
+  } 
+
+  return total / numRuns;
+}
 
 int main() {
 
-  // Array holding arrays of different sizes
-  int *arr[NUM_SIZES]; 
-  int i;
+  const int NUM_SIZES = sizeof(SIZES)/sizeof(SIZES[0]);
+  double cpu_times[NUM_SORT_FUNCS][NUM_SIZES];
+  int i, *arr;
 
-  // Allocate each array for each size
-  for (i = 0; i < NUM_SIZES; ++i)
-    arr[i] = (int *) malloc(SIZES[i] *sizeof(int));
-   
-  // Free memory
-  for (i = 0; i < NUM_SIZES; ++i) 
-    free(arr[i]);
-  
+  // For each size in SIZES
+  for (i = 0; i < NUM_SIZES; ++i) {
+    // Allocate
+    arr = (int *) malloc(NUM_SIZES * sizeof(int));
+    
+    // Time
+    cpu_times[BUBBLE][i] =
+      average_CPU_time(bubbleSort, arr, SIZES[i], NUM_RUNS);
+
+    cpu_times[COCKTAIL][i] = 
+      average_CPU_time(cocktailSort, arr, SIZES[i], NUM_RUNS);
+
+    cpu_times[SHELLSORT_1][i] =
+      average_CPU_time(shellSort_1, arr, SIZES[i], NUM_RUNS);
+
+    cpu_times[SHELLSORT_2][i] =
+      average_CPU_time(shellSort_2, arr, SIZES[i], NUM_RUNS);
+
+    // Free
+    free(arr);
+    
+  }
+
+  // Print Results
+    
+
   return 0;
 }
